85c730a8960148aff0893bf6bca56cfd97d1d07f
Miguel <migueljomartinez@gmail.com>
2023-09-01 22:48:05 -0500

    Creating function in a simpler way

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -13,5 +13,8 @@ public class MathFunctions {
 		};
 		System.out.println(square.apply(5));;
 		System.out.println(square.apply(35));;
+
+		Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+		System.out.println("3 is odd? " + isOdd.apply(3));
 	}
 }
