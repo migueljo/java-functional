589176fb1a31ea7f61d3b83ec4d088f8f750f577
Miguel <migueljomartinez@gmail.com>
2023-09-01 22:50:31 -0500

    Using a predicate function

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -1,6 +1,7 @@
 package com.platzi.functional._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
 	public static void main(String[] args) {
@@ -16,5 +17,10 @@ public class MathFunctions {
 
 		Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
 		System.out.println("3 is odd? " + isOdd.apply(3));
+		System.out.println("4 is odd? " + isOdd.apply(4));
+
+		Predicate<Integer> isEven = x -> !isOdd.apply(x);
+		System.out.println("122 is even? " + isEven.test(122));
+		System.out.println("121 is even? " + isEven.test(121));
 	}
 }
