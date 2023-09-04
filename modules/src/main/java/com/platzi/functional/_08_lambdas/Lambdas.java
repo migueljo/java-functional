5de3325ffa0ff2906de3aa075d459ac6af486988
Miguel <migueljomartinez@gmail.com>
2023-09-03 22:19:42 -0500

    Lambdas class part 2

--- modules/src/main/java/com/platzi/functional/_08_lambdas/Lambdas.java
+++ modules/src/main/java/com/platzi/functional/_08_lambdas/Lambdas.java
@@ -1,5 +1,35 @@
 package com.platzi.functional._08_lambdas;
 
+import com.platzi.functional._06_reference_operator.NamesUtils;
+
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Lambdas {
-	
+	public static void main(String[] args) {
+		List<String> courses = NamesUtils.getList("Java", "Kotlin");
+
+		courses.forEach(c -> System.out.println(c));
+		useZero(() -> 2);
+
+		usePredicate(t -> !t.isEmpty(), "Not empty");
+		usePredicate(t -> !t.isEmpty(), "");
+	}
+
+	static void useZero(ZeroArguments zeroArguments) {
+		System.out.println("Use zero: " + zeroArguments.get());
+	}
+
+	static void usePredicate(Predicate<String> predicate, String value) {
+		if (predicate.test(value)) {
+			System.out.println("Predicate is true");
+		} else {
+			System.out.println("Predicate is false");
+		}
+	}
+
+	@FunctionalInterface
+	interface  ZeroArguments {
+		int get();
+	}
 }
