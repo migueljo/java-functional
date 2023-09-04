73de1d5157e96b949173ba57d65673783af8df42
Miguel <migueljomartinez@gmail.com>
2023-09-03 22:22:29 -0500

    Lambdas class part 3

--- modules/src/main/java/com/platzi/functional/_08_lambdas/Lambdas.java
+++ modules/src/main/java/com/platzi/functional/_08_lambdas/Lambdas.java
@@ -3,6 +3,7 @@ package com.platzi.functional._08_lambdas;
 import com.platzi.functional._06_reference_operator.NamesUtils;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Lambdas {
@@ -12,20 +13,26 @@ public class Lambdas {
 		courses.forEach(c -> System.out.println(c));
 		useZero(() -> 2);
 
-		usePredicate(t -> !t.isEmpty(), "Not empty");
-		usePredicate(t -> !t.isEmpty(), "");
+		usePredicate(t -> !t.isEmpty());
+		usePredicate(t -> !t.isEmpty());
+
+		useBiFunction((x, y) -> x * y);
 	}
 
 	static void useZero(ZeroArguments zeroArguments) {
 		System.out.println("Use zero: " + zeroArguments.get());
 	}
 
-	static void usePredicate(Predicate<String> predicate, String value) {
-		if (predicate.test(value)) {
+	static void usePredicate(Predicate<String> predicate) {
+		if (predicate.test("Not empty")) {
 			System.out.println("Predicate is true");
 		} else {
 			System.out.println("Predicate is false");
 		}
+	}
+
+	static void useBiFunction(BiFunction<Integer, Integer, Integer> operation) {
+		System.out.println(operation.apply(3, 4));
 	}
 
 	@FunctionalInterface
