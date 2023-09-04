74f1cb6d0e724fc3ee839b64710445a05a3ba961
Miguel <migueljomartinez@gmail.com>
2023-09-03 21:59:33 -0500

    Working with reference operator '::'

--- modules/src/main/java/com/platzi/functional/_06_reference_operator/NamesUtils.java
+++ modules/src/main/java/com/platzi/functional/_06_reference_operator/NamesUtils.java
@@ -1,4 +1,21 @@
 package com.platzi.functional._06_reference_operator;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class NamesUtils {
+	public static void main(String[] args) {
+		List<String> teachers = getList("Nicolás", "Juan", "Carlos");
+
+		Consumer<String> printer = text -> System.out.println(text);
+		teachers.forEach(printer);
+		System.out.println("////////////////////");
+		teachers.forEach(System.out::println);
+	}
+
+	static <T> List<T> getList(T... elements) {
+		return Arrays.asList(elements);
+	}
 }
