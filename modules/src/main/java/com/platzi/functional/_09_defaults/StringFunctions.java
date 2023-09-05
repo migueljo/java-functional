e894bd74ae6ad77370036fbd0bb496d11a102c59
Miguel <migueljomartinez@gmail.com>
2023-09-04 23:50:19 -0500

    StringFunctions class part 3

--- modules/src/main/java/com/platzi/functional/_09_defaults/StringFunctions.java
+++ modules/src/main/java/com/platzi/functional/_09_defaults/StringFunctions.java
@@ -13,8 +13,22 @@ public class StringFunctions {
 		}
 	}
 
+	@FunctionalInterface
+	interface DoOperation {
+		void take(String text);
+
+		default void execute(Integer x, String text) {
+			while (x-- > 0) {
+				take(text);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		StringOperation randomNumber = () -> 4;
 		randomNumber.operate("Alumno");
+
+		DoOperation operation = (text) -> System.out.println("Text is: " + text);
+		operation.execute(5, "Miguel");
 	}
 }
