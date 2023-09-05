d373ccb37a013c2b8f60555b59355a27d03a5f1d
Miguel <migueljomartinez@gmail.com>
2023-09-04 23:54:16 -0500

    Chaining part 2

--- modules/src/main/java/com/platzi/functional/_10_chaining/Chaining.java
+++ modules/src/main/java/com/platzi/functional/_10_chaining/Chaining.java
@@ -1,4 +1,12 @@
 package com.platzi.functional._10_chaining;
 
 public class Chaining {
+	public static void main(String[] args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Hola ")
+						.append("Mundo ")
+						.append(":) ")
+						.append("adios.");
+		System.out.println(stringBuilder);
+	}
 }
