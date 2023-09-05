fb3123af39002c45f9985d00021d7f5e6ee6e220
Miguel <migueljomartinez@gmail.com>
2023-09-04 23:46:48 -0500

    default method in functional interface

--- modules/src/main/java/com/platzi/functional/_09_defaults/StringFunctions.java
+++ modules/src/main/java/com/platzi/functional/_09_defaults/StringFunctions.java
@@ -1,4 +1,20 @@
 package com.platzi.functional._09_defaults;
 
 public class StringFunctions {
+	@FunctionalInterface
+	interface StringOperation {
+		Integer getLength();
+		default void operate(String text) {
+			Integer x = getLength();
+			System.out.println("X = " + x);
+			while (x-- > 0) {
+				System.out.println(text);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		StringOperation randomNumber = () -> 4;
+		randomNumber.operate("Alumno");
+	}
 }
