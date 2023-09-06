cae4b879bdd356615633d9ec1a8841a04a8c5f07
Miguel <migueljomartinez@gmail.com>
2023-09-05 23:19:23 -0500

    Composition part 2

--- modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
+++ modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
@@ -4,16 +4,27 @@ import java.util.function.Function;
 
 public class MathOperations2 {
 	public static void main(String[] args) {
-		Function<Integer, Integer> multiplyBy3 = x -> x * 3;
+		Function<Integer, Integer> multiplyBy3 = x -> {
+			System.out.println("Multiplying 3 by x: " + x);
+			return x * 3;
+		};
 		Function<Integer, Integer> add1MultiplyBy3 =
 						multiplyBy3.compose(y -> {
 							System.out.println("Add 1 to: " + y);
 							return y + 1;
 						});
+		System.out.println(add1MultiplyBy3.apply(10));
+		System.out.println("////////////////////");
+		Function<Integer, Integer> andSquare =
+						add1MultiplyBy3.andThen(x -> {
+							System.out.println("Square of x: " + x);
+							return x * x;
+						});
+		System.out.println("And square: " + andSquare.apply(3));
+
 		Function<Integer, Integer> add3MultiplyBy3 = y -> {
 			return multiplyBy3.apply(y + 3);
 		};
-		System.out.println(add1MultiplyBy3.apply(10));
 		System.out.println(add3MultiplyBy3.apply(10));
 	}
 }
