3e83c4677b1d5890346e516e9053de44b5e010b8
Miguel <migueljomartinez@gmail.com>
2023-09-07 23:58:22 -0500

    Streams part 2

--- modules/src/main/java/com/platzi/functional/_13_streams/Streams.java
+++ modules/src/main/java/com/platzi/functional/_13_streams/Streams.java
@@ -1,4 +1,22 @@
 package com.platzi.functional._13_streams;
 
+import com.platzi.functional._06_reference_operator.NamesUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class Streams {
+	public static void main(String[] args) {
+		List<String> courses = NamesUtils.getList("Java", "Javascript", "SQL", "CSS");
+		for (String course : courses) {
+			System.out.println("Course: " + course);
+		}
+
+		Stream<String> coursesStream = Stream.of("Java", "Javascript", "SQL", "CSS");
+
+		Stream<Integer> coursesLengthStream = coursesStream.map(String::length);
+		Optional<Integer> longestCourse = coursesLengthStream.max((x, y) -> y - x);
+		System.out.println("Longest course: " + (longestCourse.orElseGet(() -> 0)) );
+	}
 }
