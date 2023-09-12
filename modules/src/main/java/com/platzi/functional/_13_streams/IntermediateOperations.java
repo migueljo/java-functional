83fc685c982d598d4507cd8560ccf61f2f10e821
Miguel <migueljomartinez@gmail.com>
2023-09-12 18:41:03 -0500

    Stream intermediate operations part 2

--- modules/src/main/java/com/platzi/functional/_13_streams/IntermediateOperations.java
+++ modules/src/main/java/com/platzi/functional/_13_streams/IntermediateOperations.java
@@ -1,4 +1,11 @@
 package com.platzi.functional._13_streams;
 
+import java.util.stream.Stream;
+
 public class IntermediateOperations {
+	public static void main(String[] args) {
+		Stream<String> initialCourses = Stream.of("Java", "Spring", "Node.JS");
+		Stream<Integer> lettersOnCourses = initialCourses.map(String::length);
+		Stream<Integer> evenLengthCourses = lettersOnCourses.filter(c -> c % 2 == 0);
+	}
 }
